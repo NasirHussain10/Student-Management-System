ae666f9ece0e393e120093d0997c0d8d6ae1a206
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-27 09:47:54 -0800

    start the program and created the menu

---
+++ finalproject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+  int main (){
+  	
+  	cout << "Welcome To Student Management System\n";
+        cout << "************************************\n";
+        cout << "1. Add Student.\n";
+        cout << "2. Search Student by Name.\n";
+        cout << "3. Search Student by RollNo.\n";
+        cout << "4. Search Student by CGPA.\n";
+        cout << "5. Update Student.\n";
+        cout << "6. Display All Students.\n";
+        cout << "7. Remove Student.\n";
+        cout << "8. Exit.\n";
+  }
